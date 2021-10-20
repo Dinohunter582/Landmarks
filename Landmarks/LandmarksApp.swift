babc2fb2f542ba906bf3ba0f5b9d4a03d4c7f4ae
Dinohunter582 <markleungjr@gmail.com>
2021-10-20 15:40:50 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mark Leung Jr on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
