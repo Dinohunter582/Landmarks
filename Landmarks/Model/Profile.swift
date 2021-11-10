d0f5d86ca2632645db68e52f01518c275124a7f5
Dinohunter582 <markleungjr@gmail.com>
2021-11-10 16:40:23 -0700

    account page
    added account page and edit feature

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Mark Leung Jr on 11/10/21.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "Mark")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { rawValue }
+    }
+}
