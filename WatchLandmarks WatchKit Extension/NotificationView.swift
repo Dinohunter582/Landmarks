e9698b271c9216df04d133792a097a73f31d3cad
Dinohunter582 <markleungjr@gmail.com>
2021-11-11 14:23:32 -0700

    watchOS app integration
    Finished page view section started implementing watchOS app

---
+++ WatchLandmarks WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Mark Leung Jr on 11/11/21.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
